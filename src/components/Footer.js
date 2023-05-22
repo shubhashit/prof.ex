53f44844a86d053fc5f79abb4a78f0243f2a7e5b
Shubhashit <shubhashitjain@gmail.com>
2023-05-22 19:47:11 +0530

    destop site responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -32,8 +32,8 @@ export default function Footer() {
             FEEDBACK
           </span>
             <form action="" onSubmit={feedbackSubmit} className='w-full flex items-start flex-col'>
-              <input className=' ml-2 mt-2 w-3/4 border-b-2 border-b-black outline-none placeholder:text-white' style={{ "background": "none" }} type="email" name="" id="" placeholder='Your email goes here' />
-              <input className=' ml-2 mt-4 h-full w-3/4 border-b-2 border-b-black outline-none placeholder:text-white' style={{ "background": "none" }} type="text" name="" id="" placeholder='Write you message' />
+              <input required className=' ml-2 mt-2 w-3/4 border-b-2 border-b-black outline-none placeholder:text-white' style={{ "background": "none" }} type="email" name="" id="" placeholder='Your email goes here' />
+              <input required className=' ml-2 mt-4 h-full w-3/4 border-b-2 border-b-black outline-none placeholder:text-white' style={{ "background": "none" }} type="text" name="" id="" placeholder='Write you message' />
             <button className=' ml-2 mt-4 border-b-2  w-20  border-b-black'>Submit</button>
             </form>
         </div>
